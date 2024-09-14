aca9aecda0ef9f6570cdef0b5e5e29c29d3d9586
Артём Куертон <artyomkuerton@icloud.com>
2024-09-14 22:49:35 +0300

    Maliont

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Maliont | Your favorite products from 2023",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Your favorite products from 2023",
+        },
+        { name: "keywords", content: "Maliont, Kuerton" },
+        { name: "author", content: "Maliont" },
+      ],
+    },
+  },
+
+  compatibilityDate: "2024-09-14",
+});
